62f0eb5d587aeb3c8e175b03635a4a8d4bf74fdd
Richard Lake <ralake1985@gmail.com>
2015-01-06 14:56:45 +0000

    Set up file structure, ready for first feature test

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+    ignore_warning: {
+      options: {
+        '-W030' : true
+        },
+      },
+    files: ['Gruntfile.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true,
+        } 
+      }  
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          quiet: false
+        },
+      src: ['spec/**/*.js']
+      }
+    },
+    mocha_casperjs: {
+      options: {
+      }, 
+      files: {
+        src: ['test/*.js'] 
+      }
+    },
+    express: {
+      options:{},
+      dev: {
+        options: {
+          script: './server.js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-casperjs');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.registerTask('default', ['express', 'mochaTest', 'mocha_casperjs']);
+
+};
