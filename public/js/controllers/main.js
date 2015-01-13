83ad603c89530e686d75b6ed544e1eafdfca8aa1
Richard Lake <ralake1985@gmail.com>
2015-01-13 14:13:36 +0000

    Refactor angular controller functions

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -2,19 +2,32 @@ angular.module("playerController", ["ngResource"])
 
   .controller('mainController', function($scope, Player) {
 
-    $scope.formData = {};
     $scope.players = Player.query();
 
     $scope.createPlayer = function() {
-      if (!$.isEmptyObject($scope.formData)) {
+      $scope._postPlayer();
+      $scope._clearForm();
+    };
+
+    $scope.newPlayerRank = function() {
+      return $scope.players.length + 1;
+    };
+
+    $scope._postPlayer = function() {
+      if (!$.isEmptyObject($scope.newPlayer)) {
         var player = new Player();
-        player.name = $scope.formData.name;
-        player.rank = ($scope.players.length + 1);
-        player.$save(player, function(){
+        player.name = $scope.newPlayer.name;
+        player.rank = $scope.newPlayerRank();
+        player.$save(function(player){
           $scope.players.push(player);
         });
-        $scope.formData = {}; 
-      };
+      }
+    };
+
+    $scope._clearForm = function() {
+      $scope.newPlayer = {};  
+      $scope.addPlayer.$setPristine();
     };
 
   });
+
