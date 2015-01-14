01ed20d57c1ebd1d8f93143eafd9a96926f21f45
Nick Dyer <nbdyer@gmail.com>
2015-01-14 12:22:53 +0000

    Write two tests for  requests of controller

--- test/controllers/playerController.spec.js
+++ test/controllers/playerController.spec.js
@@ -3,8 +3,9 @@ describe('playerController', function() {
 
   var scope, factory, ctrl;
 
-  beforeEach(inject(function($rootScope, $controller, Player) {
+  beforeEach(inject(function($rootScope, $injector, $controller, Player) {
     factory = Player;
+    $httpBackend = $injector.get('$httpBackend');
     scope = $rootScope.$new();
     ctrl = $controller('mainController', {
       $scope: scope,
@@ -12,6 +13,16 @@ describe('playerController', function() {
     });
 
   }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  }); 
+  
+  it('should get players from the db on initialisation', function() {
+    $httpBackend.expectGET('/api/players');
+    $httpBackend.flush();
+  });
 
   it('should initialise with an empty players resource', function() {
     expect(scope.players).toEqual([]);
@@ -21,18 +32,29 @@ describe('playerController', function() {
     expect(scope.newPlayerRank()).toEqual(1); 
   });
 
-  it('creates a new player', function(){
+  it('creates a new player in the view', function(){
     scope.newPlayer = { name: 'Rich', rank: 1 };
     scope.createPlayer();
     expect(scope.players).toEqual([{ name: 'Rich', rank: 1 }]);
     expect(scope.newPlayer).toEqual({});
   });
 
-  it('can swap players ranks in the ladder', function(){
+  it('can swap players ranks in the ladder in the view', function(){
     scope.winner = {name: 'Rich', rank: 2};
     scope.loser = {name: 'Nick', rank: 1};
     scope.updateLadder();
     expect(scope.winner.rank).toEqual(1);
     expect(scope.loser.rank).toEqual(2);
   });
+
+  it('creates a new player in the db', function(){
+    $httpBackend.flush();
+    $httpBackend.expectPOST('/api/players', { name: 'Rich', rank: 1 }).respond(404);
+    scope.newPlayer = { name: 'Rich', rank: 1 };
+    scope.createPlayer();
+    expect(scope.players).toEqual([{ name: 'Rich', rank: 2 }]);
+    expect(scope.newPlayer).toEqual({});
+  });
+
+
 });
