5d840356fe8cc6c0c3ccf828e1e528fbe2c5019f
Richard Lake <ralake1985@gmail.com>
2015-02-23 14:53:50 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -2,22 +2,22 @@ var PlayerRepository = require('./models/playerrepository');
 var Player = require('./models/player')
 var routes = function(app, router) {
 
-  function returnPlayers(players){ return players };
+  function returnPlayers(players){
+    return players
+  };
 
   router.route('/players')
 
     .get(function(request, response) {
       Player(PlayerRepository).getPlayers(function(err, players) {
-        if(err)
-          response.send(err)
+        if(err) response.send(err)
         response.json(players)
       });
     })
 
     .post(function(request, response){
       Player(PlayerRepository).addPlayer(request.body, function(err, player) {
-        if(err)
-          response.send(err)
+        if(err) response.send(err)
         response.json(player)
       });
     }); 
@@ -26,13 +26,11 @@ var routes = function(app, router) {
 
     .put(function(request, response) {
       Player(PlayerRepository).updatePlayer(request.body, request.params.id, function(err, player) {
-        if(err)
-          response.send(err)
+        if(err) response.send(err)
         response.json(player)
       }); 
     });
-        
-
+    
   app.use('/api', router);
  
 };
